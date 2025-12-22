9cafa2a8c1d18e2b09657708da71c3c47a67f9b8
engrron30 <ronaldtorresrosete30@gmail.com>
2025-12-22 11:11:03 +0800

    Refactor client code for readability

--- 01_Local-Sock/client.c
+++ 01_Local-Sock/client.c
@@ -7,27 +7,39 @@
 #define PORT 8080
 
 int main() {
-    int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
     char *hello = "Hello from client";
 
+    // 1. Create the client socket
+    int sock = 0;
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Socket creation error");
         return -1;
     }
 
+    // Convert IPv4 and IPv6 addresses from text to binary form
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
+    // 2. Connect to server
+    int connect_ret = 0;
+    connect_ret = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (connect_ret < 0) {
+	    printf("Connection failed!\n");
+	    return -1;
+    }
+
+    // 3. Send data to server
     send(sock, hello, strlen(hello), 0);
+    
+    // 4. Read the server's response
     read(sock, buffer, 1024);
     printf("Server: %s\n", buffer);
 
+    // Close the socket
     close(sock);
     return 0;
 }
