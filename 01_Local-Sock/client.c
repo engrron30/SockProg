99105b50b3b93deb4a0e609f58e96837748b1dd3
engrron30 <ronaldtorresrosete30@gmail.com>
2025-12-22 10:15:49 +0800

    Add client and server C code

---
+++ 01_Local-Sock/client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char *hello = "Hello from client";
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    send(sock, hello, strlen(hello), 0);
+    read(sock, buffer, 1024);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+    return 0;
+}
+
---
+++ 01_Local-Sock/server.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    char buffer[1024] = {0};
+    char *message = "Hello from server";
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    int addrlen = sizeof(address);
+    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    read(new_socket, buffer, 1024);
+    printf("Client: %s\n", buffer);
+    send(new_socket, message, strlen(message), 0);
+    printf("Message sent\n");
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
+
