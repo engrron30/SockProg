f8f43f48b6850857350ed4364beb38347d5525e4
engrron30 <ronaldtorresrosete30@gmail.com>
2025-12-23 14:03:42 +0800

    Clean server code comments

--- 02_Local-UDP/Source/server.c
+++ 02_Local-UDP/Source/server.c
@@ -8,42 +8,40 @@
 
 int main() {
     char buffer[1024];
-    struct sockaddr_in servaddr, cliaddr;
 
-    // Create UDP socket
+    // 1. Create UDP socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Prepare server and client address
+    struct sockaddr_in servaddr, cliaddr;
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
-
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
 
-    // Bind socket to IP and port
+    // 2. Bind socket to IP and port
     if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("bind failed");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-
     printf("UDP server listening on port %d...\n", PORT);
 
+    // 3. Receive data
     socklen_t len;
     int n;
-
-    // Receive data
     len = sizeof(cliaddr);
     n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
                  (struct sockaddr *)&cliaddr, &len);
     buffer[n] = '\0';
     printf("Client: %s\n", buffer);
 
-    // Send response back
+    // 4. Send response back
     const char *msg = "Hello from UDP server";
     sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&cliaddr, len);
     printf("Message sent.\n");
