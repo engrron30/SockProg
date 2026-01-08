1fe6563adea103d1871787cd9790521cf13396ea
engrron30 <ronaldtorresrosete30@gmail.com>
2026-01-08 09:14:27 +0800

    Add a string getter from user in the client side

---
+++ 03_TCP-with-Half-Duplex/Source/client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP_ADDR          "127.0.0.1"
+#define SERVER_PORT             8080
+#define CLIENT_MSG_STR          "Hello from client!"
+#define CLIENT_MSG_STR_LEN      1024
+
+int main() {
+
+    // 1. Create the client socket
+    int sock = 0;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    // Prepare the server socket struct to connect
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP_ADDR, &serv_addr.sin_addr);
+
+    // 2. Connect to server
+    int connect_ret = 0;
+    connect_ret = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (connect_ret < 0) {
+	    printf("Connection failed!\n");
+	    return -1;
+    }
+
+    // 3. Send data to server
+    char client_msg[CLIENT_MSG_STR_LEN];
+    fgets(client_msg, sizeof(client_msg), stdin);
+    client_msg[strcspn(client_msg, "\n")] = 0;
+    send(sock, client_msg, strlen(client_msg), 0);
+    
+    // 4. Read the server's response
+    char buffer[1024] = {0};
+    read(sock, buffer, 1024);
+    printf("Server: %s\n", buffer);
+
+    // Close the socket
+    close(sock);
+    return 0;
+}
+
---
+++ 03_TCP-with-Half-Duplex/Source/server.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_MSG_STR          "Hello from server"
+#define PORT                    8080
+
+int main() {
+
+    // 1. Create socket file descriptor
+    int server_fd;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+#ifdef SERVER_REUSE_ADDR
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt(SO_REUSEADDR)");
+        exit(EXIT_FAILURE);
+    }
+#endif
+
+    // 2. Bind the socket to the desired port
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. Listen for incoming connections
+    int listen_ret = listen(server_fd, 3);
+    if (listen_ret < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("[SERVER] TCP server is listening...\n");
+
+    // 4. Accept a client connection
+    int new_socket;
+    int addrlen = sizeof(address);
+    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
+    // 5. Read data from client
+    char buffer[1024] = {0};
+    read(new_socket, buffer, 1024);
+    printf("Client: %s\n", buffer);
+
+    // 6. Send a response to client
+    send(new_socket, SERVER_MSG_STR, strlen(SERVER_MSG_STR), 0);
+    printf("Message sent\n");
+
+    // 7. Close the connection
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
+
