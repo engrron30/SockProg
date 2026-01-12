85b9c0ddd833b91d5fc136889a652d82c32aa4f1
engrron30 <ronaldtorresrosete30@gmail.com>
2026-01-12 14:35:48 +0800

    Add for-loop for starting multiple clients

--- 04_TCP-Server-with-Multiple-Clients/Scripts/run_multiple_clients.sh
+++ 04_TCP-Server-with-Multiple-Clients/Scripts/run_multiple_clients.sh
@@ -2,8 +2,13 @@
 
 CURRENT_DIR=$(pwd)
 OUTPUT_DIR="${CURRENT_DIR}/Output/"
+CLIENT_NUM_MAX=10
 
 if [ ! -d "$OUTPUT_DIR" ]; then
     echo "$OUTPUT_DIR does not exists. Build the binary first."
     exit 1
 fi
+
+for i in $(seq 1 $CLIENT_NUM_MAX); do
+    echo "[CLIENT $i] Starting the client..."
+done
