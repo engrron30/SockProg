9928ee67d1f25ff2ac469267b4d6f2d9c4e07cfa
engrron30 <ronaldtorresrosete30@gmail.com>
2026-01-12 14:28:45 +0800

    Use '{}' for variable expansion in Bash shell scripts

--- 04_TCP-Server-with-Multiple-Clients/Scripts/run_multiple_clients.sh
+++ 04_TCP-Server-with-Multiple-Clients/Scripts/run_multiple_clients.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CURRENT_DIR=($pwd)
-OUTPUT_DIR="$(CURRENT_DIR)/Output/"
+CURRENT_DIR=$(pwd)
+OUTPUT_DIR="${CURRENT_DIR}/Output/"
 
 if [ ! -d "$OUTPUT_DIR" ]; then
     echo "$OUTPUT_DIR does not exists. Build the binary first."
